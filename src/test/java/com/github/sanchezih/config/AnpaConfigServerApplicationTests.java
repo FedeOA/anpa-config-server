f41697a8c1fb1a08960b3deaef8c971bb7bd37ed
Federico O. Acosta <90874585+FedeOA@users.noreply.github.com>
2026-05-17 10:45:31 -0300

    Initialize release/1.0.0

---
+++ src/test/java/com/github/sanchezih/config/AnpaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sanchezih.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnpaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
